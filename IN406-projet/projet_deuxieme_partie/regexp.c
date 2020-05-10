c32eab9ce3a8023328b34c57f94891ea2c8c9271
Emeric Gouy <emericgouy@gmail.com>
2020-05-10 20:35:16 +0200

    IN406 : clean

--- IN406-projet/projet_deuxieme_partie/regexp.c
+++ IN406-projet/projet_deuxieme_partie/regexp.c
@@ -6,8 +6,8 @@ STATELISTE table[7][7] = {//cette table représente la table des transitions de 
 		{{3,{PLUS,A,B}},{-1},{-1},{-1},{0},{-1},{1,{CAR}}},//STATE B
 		{{-1},{-1},{-1},{2,{E,F}},{-1},{2,{E,F}},{-1}},//STATE C
 		{{0},{3,{POINT,C,D}},{-1},{-1},{0},{-1},{0}},//STATE D
-		{{0},{0},{0},{3,{PARO,S,PARF}},{-1},{1,{CAR}},{-1}},//STATE E
-		{{0},{0},{2,{ETOILE,F}},{-1},{0},{-1},{0}}//STATE F
+		{{0},{0},{-1},{3,{PARO,S,PARF}},{-1},{1,{CAR}},{-1}},//STATE E
+		{{0},{0},{1,{ETOILE}},{-1},{0},{-1},{0}}//STATE F
 	};
 //Une STATELISTE de taille 0 correspond à une règle dont la production est epsilon.
 //Une STATELISTE de taille -1 correspond à une erreur (expression rejetée)
@@ -58,58 +58,80 @@ int char_indice(STATE s){
 int is_terminal(STATE s){
 	return s>6;
 }
-ADERIV creer_branches(ADERIV a, PILE *p, char **expr){
+//retourne 1 si expr est bien parenthésée, 0 sinon
+int check_pars(char *expr){
+	PILE p = nouvelle_pile(strlen(expr));
+	while(*expr){
+		if(*expr == '('){
+			p = empiler(p,PARO);
+		}else if(*expr == ')'){
+			if(est_vide(p)){
+				liberer_pile(p);
+				return 0;
+			}
+			depiler(&p);
+		}
+		expr++;
+	}
+	int ok = est_vide(p);
+	liberer_pile(p);
+	if(!ok) return 0;
+	return 1;
+}
+//retourne 1 s'il n'y a pas d'erreur de parsing, 0 sinon
+int creer_branches(ADERIV *a, PILE *p, char **expr){
+	if(!(**expr)) return 0;
+	int ok = 1;
 	STATE to_read = depiler(p);
 	if(is_terminal(to_read)){
 		int to_match = char_indice(to_read);
-		printf("terminal state : %d\n",to_read);
-		printf("to_match : %c\n",to_match);
 		if(to_match == 0){
 			if('a' <= **expr && **expr <= 'z'){
 				(*expr)++;
 			}else if(**expr == '#'){;
 			}
 			else{
-				printf("1error parsing regex");
-				exit(3);
+				ok=0;
 			}	
 		}else if(**expr == (char)to_match){
 			(*expr)++;
 		}else{
-			//erreur
-			printf("2error parsing regex");
-			exit(3);
+			ok=0;
 		}
 	}else{
-		printf("nonterminal state : %d\n",to_read);
-		printf("on read : %c\n", **expr);
 		STATELISTE to_add = table[to_read][indice_char(**expr)];
 		if(to_add.taille == -1){
-			//erreur
-			printf("3error parsing regex");
-			exit(3);	
+			ok=0;	
 		}else{
 			for(int i = 0; i<to_add.taille; i++){
 				*p = empiler(*p,to_add.liste[to_add.taille-1-i]);
-				printf("to push : %d\n", to_add.liste[to_add.taille-1-i]);
 				if(to_add.liste[i] == CAR)
-					a->fils[i] = nouvel_arbre(to_add.liste[i],**expr);
+					(*a)->fils[i] = nouvel_arbre(to_add.liste[i],**expr);
 				else
-					a->fils[i] = nouvel_arbre(to_add.liste[i],0);
+					(*a)->fils[i] = nouvel_arbre(to_add.liste[i],0);
 			}
 			for(int i = 0; i<to_add.taille; i++)
-				a->fils[i] = creer_branches(a->fils[i],p,expr);
+				ok &= creer_branches(&((*a)->fils[i]),p,expr);
 		}
 	}
-	return a;
+	return ok;
 }
+//retourne l'arbre de dérivation si l'er est valide, NULL sinon
 ADERIV construire_arbre_derivation(char *expr){
 	int taille = strlen(expr);
 	PILE p = nouvelle_pile(taille*2);
+	int pars = check_pars(expr);
+	if(!pars) return NULL;
 	ADERIV a = nouvel_arbre(S,0);
 	p = empiler(p,S);
-	a = creer_branches(a,&p,&expr);
-	return a;
+	int ok_tree = creer_branches(&a,&p,&expr);
+	int ok_pile = est_vide(p);
+	liberer_pile(p);
+	if(ok_tree && ok_pile) return a;
+	else{
+		liberer_arbre(a);
+		return NULL;
+	}
 }
 
 void affiche_aderiv(ADERIV a, int space){//rendre joli
